12a902d55f5e20bb45053b135a426efebdb522cf
schloing <user.datagram.protocol@proton.me>
2023-10-17 13:07:27 +1100

    HIGH_SPEED plotting (AGG backend)

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from   matplotlib.widgets import Button, Slider
+from   matplotlib.widgets import Slider
 
 HBAR = 1.0545718e-34  # planck's constant
 M_E  = 9.10938356e-31 # mass of an electron
 DT   = 1              # simulation timestep
 N    = 100            # number of timesteps
-L    = 15             # grid size in all dimensions
-EF   = 3              # exponentiation factor for point size in scatterplot
+L    = 25             # grid size in all dimensions
+EF   = 1              # exponentiation factor for point size in scatterplot
+
+HIGH_SPEED = True     # default (0) -> AGG (1)
+
+if HIGH_SPEED:
+    plt.switch_backend('Agg')
+    plt.ioff()
 
 class HydrogenAtom:
     def __init__(self, n, l, m):
@@ -55,6 +61,8 @@ def plot_hydrogen_wavefunction(n, l, m, threshold = 0.01):
     fig = plt.figure()
     ax  = fig.add_subplot(111, projection='3d')
 
+    ax.view_init(20.5, 45)
+
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
@@ -70,23 +78,24 @@ def plot_hydrogen_wavefunction(n, l, m, threshold = 0.01):
 
     pd_threshold = prob_density[threshold_condition]
 
-    ax_expo = plt.axes([0.25, 0.1, 0.65, 0.03])
-    expo_slider = Slider(
-        ax      = ax_expo,
-        label   = 'exponentiation factor',
-        valmin  = 0.0001,
-        valmax  = 10,
-        valinit = 3,
-    )
+    if not HIGH_SPEED:
+        ax_expo = plt.axes([0.25, 0.1, 0.65, 0.03])
+        expo_slider = Slider(
+            ax      = ax_expo,
+            label   = 'exponentiation factor',
+            valmin  = 0.0001,
+            valmax  = 10,
+            valinit = 3,
+        )
 
-    ax_thresh = plt.axes([0.25, 0.2, 0.65, 0.03])
-    thresh_slider = Slider(
-        ax      = ax_thresh,
-        label   = 'minimum threshold',
-        valmin  = 0.0001,
-        valmax  = 1,
-        valinit = threshold,
-    )
+        ax_thresh = plt.axes([0.25, 0.2, 0.65, 0.03])
+        thresh_slider = Slider(
+            ax      = ax_thresh,
+            label   = 'minimum threshold',
+            valmin  = 0.0001,
+            valmax  = 1,
+            valinit = threshold,
+        )
 
     scatter = ax.scatter(x_threshold, y_threshold, z_threshold, 
                         c = pd_threshold, cmap='viridis', s = np.minimum((pd_threshold * 1000) ** EF, 50))
@@ -123,15 +132,19 @@ def plot_hydrogen_wavefunction(n, l, m, threshold = 0.01):
         
         last_threshold = threshold
 
-    expo_slider.on_changed(update) # redraw scatterplot
-    thresh_slider.on_changed(update)
+    if not HIGH_SPEED:
+        expo_slider.on_changed(update) # redraw scatterplot
+        thresh_slider.on_changed(update)
 
-    plt.show()
+    plt.show() # won't do anything if HIGH_SPEED
 
 # constraints: n > l
 
 n = 4  # principal quantum number
-l = 3  # azimuthal quantum number
+l = 2  # azimuthal quantum number
 m = 0  # magnetic quantum number
 
-plot_hydrogen_wavefunction(n, l, m, threshold = 0.001)
+plot_hydrogen_wavefunction(n, l, m, threshold = 0.0001)
+
+if HIGH_SPEED:
+    plt.savefig('foo.png')
